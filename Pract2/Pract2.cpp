575c314537a56732057167d229f1b03ae5b4622c
Player <Player@PC>
2022-11-13 16:21:32 +0200

    Добавьте файлы проекта.

---
+++ Pract2/Pract2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+	int a = 0;
+	cout << "Введ1ть св1й в1к:";
+	cin >> a;
+
+		{
+			if ((a > 0) && (a <= 100))
+			{
+				if ((a % 10 == 0) || (a % 10 >= 5))
+					cout << "Вам " << a << " Рок1в" << endl;
+				else if (a % 10 == 1)
+					cout << "Вам " << a << " Р1к" << endl;
+				else
+					cout << "Вам " << a << " Рок1в" << endl;
+			}
+			else
+				cout << "Введ1ть значення менше 101" << endl;
+		}
+}
